4e4e94bfeedc1dfe2c04c9e822ece11326dc1d26
Esteham H. Zihad ansari <eshasan1287005@gmail.com>
2025-08-14 23:51:02 +0600

    try to CRUD first laravel CRUD so create ProductController produc migration edit web php page

---
+++ app/Http/Controllers/ProductControllers.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProducController extends Controllers
+{
+    public function index()
+    {
+        $products = Product::latest()->paginate(5);
+        return view('products.index', compact('products'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'  => 'required',
+            'price' => 'required|numeric',
+            'stoct' => 'required|integer',
+        ]);
+
+        Product::create($request->all());
+
+        return redirect()->route('products.index')
+                         ->with('success', 'Product created successfully.');
+    }
+
+    public function show(Product $product)
+    {
+        return view('products.show', compact('product'));
+    }
+
+    public function edit(Product $product)
+    {
+        return view('product.edit', compact('product'));
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name'  => 'required',
+            'price' => 'required|numeric',
+            'stoct' => 'required|integer',
+        ]);
+
+        $product->update($request->all());
+
+        return redirect()->route('products.index')
+                         ->with('success', 'Product updated successfully.');
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect()->route('products.index')
+                         ->with('success','Product deleted successfully,');
+    }
+
+
+}
