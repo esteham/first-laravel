55361a6ffd34f5c79d0efe9d10d6cf98f6035bdd
Esteham H. Zihad Ansari <eshasan1287005@gmail.com>
2025-08-18 10:50:56 +0600

    route prefix lear successfully

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\ProductController;
 
 Route::resource('products', ProductController::class);
 
+
 //2nd class Route test
 Route::get('/hello', function (){
     return "Hello spider";
@@ -29,6 +30,7 @@ Route::get('/post/{id}/{name}', function($id,$name){
     return "This post ID is :".$id. " and post Name is :".$name;
 })->where(['id'=>'[0-9]+', 'name' => '[a-zA-Z]+']);
 
+
 //Group route
 Route::group([], function(){
 
@@ -42,6 +44,24 @@ Route::group([], function(){
 
     Route::get('/third', function(){
         echo "Third Route";
+    });
+
+});
+
+
+//Route Prefix
+Route::group(['prefix' => 'Prefix'], function(){
+    Route::get('/aws', function(){
+        echo 'AWS Trainig';
+    });
+    Route::get('/laravel', function(){
+        echo 'Laravel Trainig';
+    });
+    Route::get('/c#', function(){
+        echo 'C# Trainig';
+    });
+    Route::get('/react', function(){
+        echo 'React Trainig';
     });
 
 });
