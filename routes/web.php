0e3a70862dfa9301a47db8cdade7fbec748de4f2
Esteham H. Zihad Ansari <eshasan1287005@gmail.com>
2025-08-18 10:34:38 +0600

    Demo route create successfully

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,22 @@ Route::get('/', function () {
 use App\Http\Controllers\ProductController;
 
 Route::resource('products', ProductController::class);
+
+//2nd class Route test
+Route::get('/hello', function (){
+    return "Hello spider";
+});
+
+Route::get('/redirect', function (){
+    return redirect('/hello');
+});
+
+Route::redirect('/home','/');
+
+Route::get('/hello1/{name}', function ($name){
+    return "Hello ".$name;
+});
+
+Route::get('/post/{id}/{name}', function($id,$name){
+    return "This post ID is :".$id. " and post Name is :".$name;
+})->where(['id'=>'[0-9]+', 'name' => '[a-zA-Z]+']);
