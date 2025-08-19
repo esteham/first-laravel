e7b15e1c3432264c8092f418baf42bccb7ed8309
Esteham H. Zihad Ansari <eshasan1287005@gmail.com>
2025-08-19 09:48:10 +0600

    footer added successfully

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-dark text-light py-3 mt-5">
+    <div class="container text-center">
+        <p class="mb-0">&copy; {{ date('Y') }} Laravel. All Rights Reserved.</p>
+    </div>
+</footer>
