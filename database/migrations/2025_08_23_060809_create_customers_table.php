f0970b53d7b08083811752def7521aed8adf0a67
Esteham H. Zihad Ansari <eshasan1287005@gmail.com>
2025-08-24 10:31:11 +0600

    updated customer column added photo and phone

--- database/migrations/2025_08_23_060809_create_customers_table.php
+++ database/migrations/2025_08_23_060809_create_customers_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
             $table->text('bio');
             $table->timestamps();
         });
